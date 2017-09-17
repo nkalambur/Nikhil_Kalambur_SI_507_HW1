6cfb8dc074db5214e631c4c4bd1debaadc1ebc7e
nkalambur <kalambur@umich.edu>
2017-09-17 13:51:18 -0400

    further updates to testing py file

--- SI507F17_project1_tests.py
+++ SI507F17_project1_tests.py
@@ -12,31 +12,27 @@ import helper_functions
 ###########
 
 class Card_Tests(unittest.TestCase):
-	# Testing the Card class
-	# Start with setup()
-	def setup(self):
-		pass # for now
-
 	# Test1: make sure the right suits are available
 	def test_available_suits(self):
 		c = Card()
 		expected_suits = ['Diamonds', 'Clubs', 'Hearts', 'Spades']
 		self.assertEqual(c.suit_names, expected_suits, "Test Suits")
+
 	# Test2: make sure the right face cards are available
 	def test_face_cards(self):
 		fc = Card(0,1)
 		self.assertEqual(str(fc), "Ace of Diamonds", "Test face card translation")
+		# This fails!
+
 	# Test3: make sure the right number of cards/suit are available
 	def test_rank_levels(self):
 		rl = Card()
 		expected_ranks = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 		self.assertEqual(rl.rank_levels, expected_ranks, "Testing the provided rank levels")
-	# End with teardown()
+
 
 class Deck_Tests(unittest.TestCase):
-# 	# Testing the Deck class
-	def setup(self):
-		pass
+ 	# Testing the Deck class
 
 	#Test that the first card in the deck is the Ace of Diamonds
 	def test_unshuffled_first_card(self):
@@ -79,30 +75,47 @@ class Deck_Tests(unittest.TestCase):
 		self.assertTrue(str(d) == str(dd), "Testing that sorting the decks works")
 
 	def test_replace_card(self):
+		# This test checks that if after popping the 1 of Diamonds and replacing it using the
+		# replace_card function, that it gets added back to the list! Works
 		d = Deck()
 		dd = Deck()
 		replace_this_card = d.cards[0] # storing 1 of diamonds in str
 		d.pop_card(0) # popping this out
 		self.assertFalse(str(d.cards[0]) == str(replace_this_card), "Should be false")
 
-		print(str(replace_this_card))
-		print(str(d.cards[0]))
+		#print(str(replace_this_card))
+		#print(str(d.cards[0]))
 
 		d.replace_card(replace_this_card)
-		# d.sort_cards()
+		d.sort_cards()
 		list_of_cards = [str(c) for c in d.cards]
+		#print(str(d.cards[0]))
 		self.assertTrue(str(replace_this_card) in list_of_cards, "True")
-		#self.assertTrue(str(d.cards[0]) == str(replace_this_card), "Should be true!")
+		self.assertTrue(str(d.cards[0]) == str(replace_this_card), "Should be true!")
 
 	def test_replace_card2(self):
-		# d = Deck()
-		# dd = Deck()
+		# Test to see if adding an existing card to a deck adds multiple list elements
+		d = Deck()
+		dd = Deck()
 
-		# try_this = d.cards[0]
-		# dd.replace_card(try_this)
+		try_this = d.cards[0]
+		tt = str(try_this)
+		dd.replace_card(try_this)
+		l_of_cards = [str(c) for c in dd.cards]
+		# print(l_of_cards.count(tt))
+		self.assertEqual(l_of_cards.count(tt), 1, "Testing that replace card didn't add existing element")
+
+	def test_deal_hand(self): # add assertEqual etc later
+		d = Deck()
+
+		my_hand = [str(c) for c in d.deal_hand(4)]
+
+		self.assertEqual(len(my_hand), 4, "Testing that full deck deals hand size of 4")
+
+	def test_deal_hand2(self):
 		pass
 
-	def test_deal_hand(self):
+	def test_deal_hand3(self):
 		# Check if 1 person can receive a 52 card deck! 
 		d = Deck()
 
@@ -115,30 +128,35 @@ class Deck_Tests(unittest.TestCase):
 		except IndexError:
 			assert False
 
-	def test_deal_hand2(self): # add assertEqual etc later
-		d = Deck()
-
-		my_hand = d.deal_hand(4)
-
-		print([str(c) for c in my_hand])
-
 class War_Game_Tests(unittest.TestCase):
  	# Testing the War Game function
+ 	# Test that the play war game function returns a tuple
 	def test_war(self):
 		outcome = play_war_game(True)
 		# print(outcome)
 		self.assertEqual((type(outcome)), tuple)
 
+	# Test that first element of tuple is in ['Player1', 'Player2', 'Tie']
 	def test_war2(self):
 		outcome = play_war_game(True)
 		self.assertTrue(outcome[0] in ['Player1', 'Player2', 'Tie'])
 
+	# Test that 2nd element is an integer
+	def test_war3(self):
+		outcome = play_war_game(True)
+		self.assertEqual(type(outcome[1]), int)
+
+	# Test that 3rd element is an integer
+	def test_war4(self):
+		outcome = play_war_game(True)
+		self.assertEqual(type(outcome[2]), int)
+
 class TestShowSong(unittest.TestCase):
 	
-	def test_show_song(self) :  # Fails
+	def test_show_song(self) :
 		s = show_song("David Bowie")
 		self.assertEqual(s.artist, "David Bowie", "Testing that search term is in the string method")
-		# print('\n'+s.artist)
+		# This Fails!
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
